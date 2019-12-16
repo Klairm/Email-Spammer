286c32d980364deeed539e88b33c7792740cc15a
Ivan Di Scipio <40839469+Klairm@users.noreply.github.com>
2019-12-16 21:20:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
-apt-get update -y          # Update
-apt-get autoremove         # Auto Remove
-
-sudo "apt-get install pip" # Installing Pip
-echo installed pip         # Telling User that installing is done
-
+echo "Arch[1], Debian-based[2],Fedora[3]"
+read op
+if [ $op == 1]; then
+    sudo pacman -Syu
+    sudo pacman -Scc
+    sudo pacman -S python-pip
+    sudo pacman -S python2-pip
+elif [ $op == 2]; then
+    sudo apt update
+    sudo apt autoremove
+    sudo apt install python-pip
+    sudo apt install python2-pip
+elif [ $op == 3]; then      
+    sudo dnf upgrade 
+    sudo dnf clean all
+    sudo dnf install python2-pip
+    sudo dnf install python2-pip
+else
+    echo "Wrong option"
+    
 pip install pysocks        # Installing pysocks
 echo "installed pysocks"   # Teling User that installing is done
 
